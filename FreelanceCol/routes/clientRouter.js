e88f49b0a474844b3dee49cc520d2345f152d1f9
OscarATovar <jhucaron@gmail.com>
2022-12-04 11:45:03 -0500

    Subida de prueba

---
+++ FreelanceCol/routes/clientRouter.js
@@ -0,0 +1,22 @@
+import express from "express"
+
+const clientRouter = express.Router()
+
+//CRUD
+//CREAR: POST
+clientRouter.post("/nuevo", (req, res) => {
+
+})
+
+//LEER: GET
+clientRouter.get("/nuevo", (req, res) => {
+
+})
+//ACTUALIZAR: PUT/ PATCH
+clientRouter.patch("/nuevo", (req, res) => {
+
+})
+//ELIMINAR: DELETE
+clientRouter.delete("/nuevo", (req, res) => {
+
+})
